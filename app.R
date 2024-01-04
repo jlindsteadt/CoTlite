5fc46b2751366c228ecba0058ae5ce1975f6f0bc
jlindsteadt <joshlindsteadt@gmail.com>
2024-01-03 20:27:15 -0700

    initial commit

---
+++ app.R
@@ -0,0 +1,26 @@
+library(shiny)
+library(shinydashboard)
+library(Quandl)
+library(timetk)
+library(dplyr)
+library(ggplot2)
+library(tidyr)
+library(forcats)
+
+source('mod_data_import.R')
+source('mod_COT.R')
+
+
+ui <- fluidPage(
+  mod_CoT_ui("CoT_1")
+)
+
+server <- function(input, output, session) {
+  r <- reactiveValues()
+
+  mod_data_import_server("data_import", r=r)
+  mod_CoT_server('CoT_1', r=r)
+}
+
+shinyApp(ui, server)
+
